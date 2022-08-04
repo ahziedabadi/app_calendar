898ccca302d105e8e5a77caae0a388c9b2fa2a57
amir <ah.ziedabadi@gmail.com>
2022-08-04 20:01:12 +0430

    change settings to a 3 state (base, development, production).

--- calendar_app_dev/wsgi.py
+++ calendar_app_dev/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'calendar_app_dev.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'calendar_app_dev.settings.development')
 
 application = get_wsgi_application()
