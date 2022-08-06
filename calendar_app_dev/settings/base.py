d1f950ebb587dce74e07c971a34bf90428eb7d56
amir <ah.ziedabadi@gmail.com>
2022-08-06 09:16:25 +0430

    fix some typo and adding logger to project.

--- calendar_app_dev/settings/base.py
+++ calendar_app_dev/settings/base.py
@@ -5,6 +5,8 @@ using Django 3.2.7.
 import environ
 from pathlib import Path
 
+
+
 env = environ.Env(DEBUG=(bool,False))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -135,3 +137,55 @@ MEDIA_ROOT= BASE_DIR / 'mediafiles'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# logging section
+import logging
+import logging.config
+from django.utils.log import DEFAULT_LOGGING
+
+logger = logging.getLogger(__name__)
+
+LOG_LEVEL = 'INFO'
+
+# logging config *https://docs.python.org/3/library/logging.config.html#logging-config-dictschema*
+# asctime: add time to each massage. name and level: return package name and level(warnings or error or ets) to massage.
+# 12s and 8s are space between each.
+logging.config.dictConfig({
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'console': {
+            'format':"%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
+        },
+        'file': {
+            'format':"%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
+        },
+        'django.server': DEFAULT_LOGGING['formatters']['django.server'],
+    },
+    'handlers':{
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'console',
+        },
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'formatter': 'file',
+            'filename': 'logs/calendar_app_dev.log',
+        },
+        'django.server': DEFAULT_LOGGING['handlers']['django.server'],
+    },
+    'loggers': {
+        '': {
+            'level': 'INFO',
+            'handlers': ['console','file'],
+            'propagate': False,
+        },
+        'apps': {
+            'level': 'INFO',
+            'handlers': ['console','file'],
+            'propagate': False,
+        },
+        'django.server': DEFAULT_LOGGING['loggers']['django.server'],
+    },
+})
