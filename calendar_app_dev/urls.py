88c2c269517ee6ef6601b3f41bf6790a6d435ad6
amir <ah.ziedabadi@gmail.com>
2022-08-09 13:43:55 +0430

    feature: add custom user model.

--- calendar_app_dev/urls.py
+++ calendar_app_dev/urls.py
@@ -6,5 +6,10 @@ from django.urls import path,include
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('amir/', admin.site.urls),
+    path("secret/", admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+admin.site.site_header = "Calendar_APP Admin"
+admin.site.site_title = "Calendar_APP Admin Portal"
+admin.site.index_title = "Welcome to Calendar_APP Portal"
