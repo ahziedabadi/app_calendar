531e7554362ba9fe929cd962e99df7397a5b2d5f
amir <ah.ziedabadi@gmail.com>
2022-08-19 13:12:35 +0430

    chore: setup dockerfile and docker-compose file

--- calendar_app_dev/urls.py
+++ calendar_app_dev/urls.py
@@ -10,8 +10,7 @@ urlpatterns = [
     path("api/v1/auth/", include("djoser.urls")),
     path("api/v1/auth/", include("djoser.urls.jwt")),
     path("api/v1/profile/", include("apps.profiles.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+]
 
 admin.site.site_header = "Calendar_APP Admin"
 admin.site.site_title = "Calendar_APP Admin Portal"
